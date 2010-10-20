bfc13167d8b10a38dd22a5cff91b4b92f13c0698
cedricmauclair <cedric.mauclair@gmail.com>
2010-10-20 09:30:52 +0200

    Don't remember what changed.

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((eval . (set-regexp-face "^\s*;+ ?=+\\( [ [:graph:]]+\\)?\\( =+ ?;*\\)?$" 'VioletRed4-bold-italic))
+         (eval . (set-regexp-face "^\s*;+{* \\*+ [ [:graph:]]+ \\*+ ?}*$" 'Green4-bold-italic))
+         (eval . (set-regexp-face "^\s*;+ ?\\[+ [ [:graph:]]+ \\]+$" 'h00688b-bold)))))
